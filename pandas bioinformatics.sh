3961d915c0838100fc22a042a02ca57b4d2c1fca
nxstiaa <81431912+nxstiaa@users.noreply.github.com>
2023-02-06 23:42:04 -0500

    Add files via upload

---
+++ pandas bioinformatics.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sed '/Zoo/d' pandas.txt >> no_zoo_pandas.txt
+awk '{print $1,$4,$2}' pandas.txt | sort -k3 -o special_pandas.txt
+sort -k5 pandas_2.txt >> alphabetically_sorted_zoo.txt
+sed 's/\<male\>/M/g; s/\<female\>/F/g' pandas.txt >> FM_pandas.txt
+grep "female" pandas.txt | grep -c "October"
+cat -n pandas.txt >> LineNumbered_pandas.txt
+sed -n '1p; 6p; 17p' pandas.txt >> 1_6_17_Pandas
+grep -i '[X]' pandas.txt >> xpandas.txt
+sort -k5 pandas.txt | uniq -f 4 -u pandas.txt >> pandas9.txt
+awk '{print $1,$4}' pandas.txt | awk '$2 <= 10' | sort -k2 -n >> Panda10.txt
